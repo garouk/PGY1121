4ff0a9e732b9d817f9f723640c65d2a3274262af
garouk <134543003+garouk@users.noreply.github.com>
2023-05-24 14:53:00 -0400

    archivos con validaciones incorporadeush

--- Class12/datos.py
+++ Class12/datos.py
@@ -2,12 +2,24 @@
 
 print("Datos personales")
 print(" ")
+s = 1
 vNom = input("Ingrese su nombre: ")
 print(" ")
-vEdad = input("Ingrese su edad: ")
+while s==1: 
+
+    try:
+     vEdad = int(input("Ingrese su edad: "))
+     break
+    except Exception as vEr:
+        print("Ingrese una edad valida")
+
 print(" ")
 print(f"Su nombre es: {vNom} \nSu edad es: {vEdad}")
+D = int(input("1 Si desea continuar, 2 Si desea salir: "))
+if D == 2:
+       s = 2
 
+    
 print("Programa finalizado")
 
 
