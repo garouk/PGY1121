dd20d82871c38fc223e3fb8547039e9b9011d20d
garouk <134543003+garouk@users.noreply.github.com>
2023-05-24 14:34:17 -0400

    Archivo datos.py sin validar

---
+++ Class12/datos.py
@@ -0,0 +1,14 @@
+# Ejemplo de uso de repositorio
+
+print("Datos personales")
+print(" ")
+vNom = input("Ingrese su nombre: ")
+print(" ")
+vEdad = input("Ingrese su edad: ")
+print(" ")
+print(f"Su nombre es: {vNom} \nSu edad es: {vEdad}")
+
+print("Programa finalizado")
+
+
+
